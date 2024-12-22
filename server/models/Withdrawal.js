e79d1d051908ca181c21f3f531cec2fbee5c8a5d
Samrjeet001 <Samrjeetzpm@gmail.com>
2024-12-22 12:02:00 +0530

    Update Withdrawal.js

--- server/models/Withdrawal.js
+++ server/models/Withdrawal.js
@@ -5,8 +5,16 @@ const WithdrawalSchema = new mongoose.Schema({
     email: { type: String, required: true },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User ', required: true },
     amount: { type: Number, required: true },
+    type: { type: String, default: 'Withdraw' },
     actualPayment: { type: Number, required: true },
     createdAt: { type: Date, default: Date.now },
+    upiId: { type: String, default: '' }, // UPI ID
+    mobileNumber: { type: String, default: '' }, // Mobile Number
+    accountHolder: { type: String, default: '' }, // Account Holder Name
+    accountNumber: { type: String, default: '' }, // Account Number
+    bankName: { type: String, default: '' }, // Bank Name
+    ifscCode: { type: String, default: '' }, // IFSC Code
+    branchAddress: { type: String, default: '' },
     status: { type: String, default: 'Pending' } // You can set the initial status to 'Pending'
 });
 
