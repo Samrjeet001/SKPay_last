34fb3f86f865aeb5662145242812ed7ff282c47f
Samrjeet001 <Samrjeetzpm@gmail.com>
2025-01-09 08:45:24 +0530

    Update Static.js

--- server/models/Static.js
+++ server/models/Static.js
@@ -1,7 +1,20 @@
+// import mongoose from 'mongoose';
+
+// const staticSchema = new mongoose.Schema({
+//     currentrate: { type: Number, default: 0 },
+// }, { timestamps: true }); // Automatically manage createdAt and updatedAt fields
+
+// const StaticVaraible = mongoose.model('static', staticSchema); // Removed extra space in 'User  '
+
+// export default StaticVaraible;
+
+
 import mongoose from 'mongoose';
 
 const staticSchema = new mongoose.Schema({
     currentrate: { type: Number, default: 0 },
+    address:{type: String,default : 0},
+    network:{type: String,default : 0},
 }, { timestamps: true }); // Automatically manage createdAt and updatedAt fields
 
 const StaticVaraible = mongoose.model('static', staticSchema); // Removed extra space in 'User  '
